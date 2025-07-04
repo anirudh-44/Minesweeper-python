293ab0134322680da54897da9f172068d09ba407
Anirudh G <anirudhgadekar44@gmail.com>
2025-07-04 22:05:21 +0530

    created window

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from tkinter import *
 
 root = Tk()
-
-root.mainloop()
+root.configure(bg="black")
+root.geometry('1440x720')
+root.title("Minesweeper")
+root.resizable(False, False)
+root.mainloop() 
