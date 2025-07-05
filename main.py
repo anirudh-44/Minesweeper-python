58ed7811678aad667617a0b9a6a8b49a082e9fc9
Anirudh G <anirudhgadekar44@gmail.com>
2025-07-05 22:49:50 +0530

    created top frame

--- main.py
+++ main.py
@@ -5,4 +5,13 @@ root.configure(bg="black")
 root.geometry('1440x720')
 root.title("Minesweeper")
 root.resizable(False, False)
+
+top_frame = Frame(
+    root,
+    bg='red', # change later to black
+    width = 1440,
+    height = 180
+)
+top_frame.place(x=0,y=0)
+# Run the window
 root.mainloop() 
