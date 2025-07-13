00450e1f89a2a9579377e04ef69feeaf30b3bd09
Anirudh G <anirudhgadekar44@gmail.com>
2025-07-13 23:35:55 +0530

    created cell class

--- cell.py
+++ cell.py
@@ -1,1 +1,3 @@
- 
+class Cell:
+    def __init__(self, is_mime = False):
+        self.is_mine = is_mime
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from cell import Cell
 import settings
 import utils
 
