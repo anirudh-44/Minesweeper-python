7247b21a400dc331c6b5458c3027659f3e5c2d98
Anirudh G <anirudhgadekar44@gmail.com>
2025-07-17 22:31:23 +0530

    added right click event

--- cell.py
+++ cell.py
@@ -11,7 +11,11 @@ class Cell:
             text='Cell'
         )
         btn.bind('<Button-1>', self.left_click_actions)  #<Button-1> this is the convention in tkinter for left click also, note that we are just passing the method reference and not calling it.
+        btn.bind('<Button-3>', self.right_click_actions) #right click
         self.cell_btn_object = btn
 
     def left_click_actions(self, event):
         print("left button clicked!")
+
+    def right_click_actions(self, event):
+        print("right button clicked!")
