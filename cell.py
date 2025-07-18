7597f40c99d0667347ff2f56236d952903c9336c
Anirudh G <anirudhgadekar44@gmail.com>
2025-07-18 21:41:41 +0530

    added more attributes to cell class

--- cell.py
+++ cell.py
@@ -1,14 +1,18 @@
 from tkinter import Button
 
 class Cell:
-    def __init__(self, is_mime = False):
+    def __init__(self, x, y, is_mime = False):
         self.is_mine = is_mime
         self.cell_btn_object = None
+        self.x = x
+        self.y = y
 
     def create_btn_object(self, location):
         btn = Button(
             location,
-            text='Cell'
+            width = 12,
+            height = 4,
+            text = f'{self.x},{self.y}' 
         )
         btn.bind('<Button-1>', self.left_click_actions)  #<Button-1> this is the convention in tkinter for left click also, note that we are just passing the method reference and not calling it.
         btn.bind('<Button-3>', self.right_click_actions) #right click
